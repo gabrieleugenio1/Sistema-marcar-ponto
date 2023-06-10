2385c73dbf6565941a0205ba0fe969e017d502bf
gabrieleugenio1 <101233631+gabrieleugenio1@users.noreply.github.com>
2023-06-10 10:18:40 -0300

    Correção validações

--- functions/validarFuncionario.js
+++ functions/validarFuncionario.js
@@ -11,7 +11,6 @@ module.exports = async (funcionario, tipo, matriculaOriginal) => {
   let setor = funcionario.setor;
   let cargaHorariaSemanal = parseInt(funcionario.cargahorariasemanal);
   let ativo = funcionario.ativo;
-  let cpfExiste;
 
   /** INICIO DAS VALIDAÇÕES **/      
   let erros = [];     
@@ -37,11 +36,11 @@ module.exports = async (funcionario, tipo, matriculaOriginal) => {
     let cpfExiste = await Funcionarios.findOne({raw:true, where: {cpf: cpf}});
     cpf = cpf.trim(); // Limpa espaços em branco no inicio e final do cpf.}
     /*  cpf = cpf.replace(/[^\d]/g, ''); */ // Remove caracteres não numéricos. 
-    if (!validateCPF(cpf) || !cpf || cpf == undefined || cpf == null) {
+    if (!validateCPF(cpf) || !cpf || cpf === undefined || cpf === null) {
       erros.push({ error: "CPF inválido!" });
      };    
     if(cpfExiste) {
-      if(cpfExiste.matricula != matriculaOriginal) erros.push({ error: "CPF já existe!" });
+      if(cpfExiste.matricula !== matriculaOriginal) erros.push({ error: "CPF já existe!" });
     };
   };
   
@@ -49,32 +48,32 @@ module.exports = async (funcionario, tipo, matriculaOriginal) => {
     erros.push({error: "Matrícula inválida! Deve ser apenas números."});
   };
   
-  if(!nome || nome == undefined || nome == null ) {
+  if(!nome || nome === undefined || nome === null ) {
    erros.push({error: "Nome inválido! Não pode ser vazio e deve ser completo."});
   };  
 
-  if (!email || email == undefined || nome == null || !regex.email.test(email)) {
+  if (!email || email === undefined || email === null || !regex.email.test(email)) {
    erros.push({ error: "E-mail inválido!" });
   };     
 
-  if (!cargaHorariaSemanal || cargaHorariaSemanal == undefined || cargaHorariaSemanal > 44 || isNaN(cargaHorariaSemanal) || cargaHorariaSemanal == null) {
+  if (!cargaHorariaSemanal || cargaHorariaSemanal === undefined || cargaHorariaSemanal > 44 || isNaN(cargaHorariaSemanal) || cargaHorariaSemanal === null) {
     erros.push({ error: "Carga horária inválida!" });
   }; 
 
-  if (!funcao || funcao == undefined ||funcao == null) {
+  if (!funcao || funcao === undefined ||funcao === null) {
     erros.push({ error: "Função inválida!" });
   }; 
 
-  if (!setor || setor == undefined ||setor == null) {
+  if (!setor || setor === undefined ||setor === null) {
    erros.push({ error: "Setor inválido!" });
   }; 
   
-  if (ativo == undefined || ativo == null || (ativo !== false && ativo !== true )) {
+  if (ativo === undefined || ativo === null || (ativo !== false && ativo !== true )) {
    erros.push({ error: "Ativo inválido!" });
   }; 
 
-  if(tipo == "cadastro") {
-    if(!senha || senha == undefined || senha == null || senha <= 6 ) {
+  if(tipo === "cadastro") {
+    if(!senha || senha === undefined || senha === null || senha <= 6 ) {
       erros.push({error: "Senha inválida! A senha deve ter no minimo 6 caracteres."});
     };
   };
